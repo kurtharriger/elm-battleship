c44e9c4c857b6a44ecf9ef389aec34de41133879
Kurt Harriger <kharriger@rallydev.com>
2015-11-28 15:59:35 -0700

    Use foldp' to create initial model

--- src/BattleshipNoStart.elm
+++ src/BattleshipNoStart.elm
@@ -1,42 +1,21 @@
 module Battleship where
 
 import Html
-import Signal exposing (Mailbox, Address, Signal, message, mailbox)
+import Signal exposing (Mailbox, Address, Signal, message, mailbox, foldp)
+import Signal.Extra exposing (foldp')
 import BattleshipView
 import BattleshipModel exposing (..)
-import StartApp.Simple as StartApp
-
 
 view : Address GameModelAction -> GameModel -> Html.Html
 view address =
   BattleshipView.view (message address)
-
-
-maybeView : Address GameModelAction -> Maybe GameModel -> Html.Html
-maybeView address model =
-  case Debug.log "model" model of
-    Just model -> view address model
-    Nothing -> Html.text "Model did not initialize. Check console for log output."
-
-update : GameModelAction -> Maybe GameModel -> Maybe GameModel
-update action model =
-  case Debug.log "update" (model, action) of
-    (Nothing, Initialize seed) -> Just <| initModel seed
-    (Nothing, _) -> Nothing
-    (Just model, _) -> Just <| BattleshipModel.updateGameModel action model
-
-app : StartApp.Config (Maybe GameModel) GameModelAction
-app = {
-    model = Nothing,
-    view = maybeView,
-    update = update
-  }
 
 main : Signal Html.Html
 main =
   let gameMailbox = mailbox NoOp
   in
   Signal.map
-    (app.view gameMailbox.address)
-    (Signal.foldp app.update app.model
-      (Signal.map Initialize initialSeed))
+    (view gameMailbox.address)
+    (foldp' (snd >> updateGameModel)
+      (\(seed, action) -> Debug.log "initilize" (initModel seed))
+      (Signal.map2 (,) initialSeed gameMailbox.signal))
--- src/TestInitialSeed.elm
+++ src/TestInitialSeed.elm
@@ -2,6 +2,8 @@
 import Html
 import Random
 import Time
+import Signal.Extra exposing (foldp', fairMerge)
+import Mouse
 
 initialSeed : Signal Random.Seed
 initialSeed =
@@ -10,16 +12,41 @@ initialSeed =
     (Time.timestamp (Signal.constant ()))
 
 
-main : Signal Html.Html
+type Action
+  = Initialize Random.Seed
+  | Mouse Bool
 
+--
 -- main =
 --   Signal.map
 --     (Html.text << toString)
 --     initialSeed
-
+--
+-- main =
+--    Signal.map
+--      (Html.text << toString)
+--     (Signal.foldp (\a s -> Just (Debug.log "action" a))
+--         Nothing
+--         initialSeed)
+--
+-- main =
+--   Signal.map
+--     (Html.text << toString)
+--     (foldp' (\a s -> Just (Debug.log "action" a))
+--       (Debug.log "initialize" >> Just)
+--       (Signal.mergeMany [(Signal.map Mouse Mouse.isDown), (Signal.map Initialize initialSeed)]))
+--
+-- main =
+--   Signal.map
+--     (Html.text << toString)
+--     (foldp' (\a s -> Just (Debug.log "action" a))
+--       (Debug.log "initialize" >> Just)
+--       (Signal.mergeMany [(Signal.map Initialize initialSeed), (Signal.map Mouse Mouse.isDown)]))
+--
+--
 main =
   Signal.map
     (Html.text << toString)
-    (Signal.foldp (\a s -> Just (Debug.log "action" a))
-      Nothing
-      initialSeed)
+    (foldp' (\a s -> Just (Debug.log "action" a))
+      (Debug.log "initialize" >> Just)
+      (Signal.map2 (,) (Signal.map Mouse Mouse.isDown)  (Signal.map Initialize initialSeed)))
