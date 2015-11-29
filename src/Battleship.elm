09df939d87ff2b26928b90c76452180546aa9733
Kurt Harriger <kharriger@rallydev.com>
2015-11-28 20:24:34 -0700

    Create initial model with Random.initialSeed

--- src/Battleship.elm
+++ src/Battleship.elm
@@ -1,22 +1,21 @@
 module Battleship where
 
 import Html
-import Signal exposing (Address, Signal, message)
+import Signal exposing (Mailbox, Address, Signal, message, mailbox, foldp)
+import Signal.Extra exposing (foldp')
 import BattleshipView
 import BattleshipModel exposing (..)
-import StartApp.Simple exposing (start)
-import Random
-
 
 view : Address GameModelAction -> GameModel -> Html.Html
 view address =
   BattleshipView.view (message address)
 
-
 main : Signal Html.Html
 main =
-  start {
-    model = initModel (Random.initialSeed 42),
-    view = view,
-    update = updateGameModel
-  }
+  let gameMailbox = mailbox NoOp
+  in
+  Signal.map
+    (view gameMailbox.address)
+    (foldp' (snd >> updateGameModel)
+      (\(seed, action) -> Debug.log "initilize" (initModel seed))
+      (Signal.map2 (,) initialSeed gameMailbox.signal))
